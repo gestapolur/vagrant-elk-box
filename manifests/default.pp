ef8c4cca85e90fa9ba44ec9f899629e85487db56
Gestalt LUR <gestapolur@users.noreply.github.com>
2016-03-30 16:24:26 +0800

    configure node name & remove logdash/kibana/marvel install

--- manifests/default.pp
+++ manifests/default.pp
@@ -11,62 +11,23 @@ class { 'java': }
 class { 'elasticsearch':
   manage_repo  => true,
   repo_version => '1.7',
+  # version => '1.7.5'
 }
 
-elasticsearch::instance { 'es-01':
+elasticsearch::instance { $node_name:
   config => { 
   'cluster.name' => 'vagrant_elasticsearch',
   'index.number_of_replicas' => '0',
   'index.number_of_shards'   => '1',
   'network.host' => '0.0.0.0',
-  'marvel.agent.enabled' => false #DISABLE marvel data collection. 
   },        # Configuration hash
-  init_defaults => { }, # Init defaults hash
-  before => Exec['start kibana']
+  init_defaults => { } # Init defaults hash
 }
 
 elasticsearch::plugin{'royrusso/elasticsearch-HQ':
-  instances  => 'es-01'
+  instances  => $node_name
 }
 
-elasticsearch::plugin{'elasticsearch/marvel/latest':
-  instances  => 'es-01'
-}
-
-# Logstash
-class { 'logstash':
-  # autoupgrade  => true,
-  ensure       => 'present',
-  manage_repo  => true,
-  repo_version => '1.5',
-  require      => [ Class['java'], Class['elasticsearch'] ],
-}
-
-# remove initial logstash config
-#file { '/etc/logstash/conf.d/logstash':
-  #ensure  => '/vagrant/confs/logstash/logstash.conf',
- # require => [ Class['logstash'] ],
-#}
-
-
-# Kibana
-package { 'curl':
-  ensure  => 'present',
-  require => [ Class['apt'] ],
-}
-
-file { '/opt/kibana':
-  ensure => 'directory',
-  group  => 'vagrant',
-  owner  => 'vagrant',
-}
-
-exec { 'download_kibana':
-  command => '/usr/bin/curl -L https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz | /bin/tar xvz -C /opt/kibana --strip-components 1',
-  require => [ Package['curl'], File['/opt/kibana'], Class['elasticsearch'] ],
-  timeout => 1800
-}
-
-exec {'start kibana':
-    command => '/etc/init.d/kibana start',
+elasticsearch::plugin{'mobz/elasticsearch-head':
+  instances => $node_name
 }
